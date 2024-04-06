2ef5045acc1d6b48a76a5650c4a82900b1abc095
Divyesh Vishwakarma <divyesh1099@gmail.com>
2024-04-06 18:07:51 +0530

    Remove Unused Code

--- bioForge/cover_letter/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class CoverLetterConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'cover_letter'
--- bioForge/home/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Profile, Education, Experience, Skill, Project, AwardCertification
-
-admin.site.register(Profile)
-admin.site.register(Education)
-admin.site.register(Experience)
-admin.site.register(Skill)
-admin.site.register(Project)
-admin.site.register(AwardCertification)
